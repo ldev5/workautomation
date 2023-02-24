e6164f5fd18c20503607d80271795dae41ad71c7
ldev5 <dhayes11@proton.me>
2023-02-23 20:49:47 -0600

    modified:   Job1/1 Computer Startup Scripts/Server/start-dnivm01.ps1

--- Job1/1 Computer Startup Scripts/Server/start-dnivm01.ps1
+++ Job1/1 Computer Startup Scripts/Server/start-dnivm01.ps1
@@ -1,2 +1,3 @@
-# Start up the Virtual Machine "dnivm01" on Hyper-V used for running Windows software. Uses the parameter "AsJob" to start in the background instead of opening the actual instance in a window.
+# Start up the Virtual Machine "dnivm01" on Hyper-V used for running Windows software. 
+# Uses the parameter "AsJob" to start in the background instead of opening the actual instance in a window.
 Start-VM -Name dnivm01 -AsJob
